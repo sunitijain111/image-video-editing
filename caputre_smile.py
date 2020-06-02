b7a52a22f39852f0bd340ec7de5cd441484f8f51
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-06-02 14:12:23 +0530

    capture smile and save it

---
+++ caputre_smile.py
@@ -0,0 +1,54 @@
+
+import cv2
+import datetime
+
+
+#these are the cascades: thay are already present in the opencv
+cascade_face = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml') 
+cascade_eye = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml') 
+cascade_smile = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+
+#function ot detect teh face
+
+# gray image of our face, original image:
+def detection(grayscale, img):
+    
+    #1.3 is the scale factor , 5 is te min neighbours
+    face = cascade_face.detectMultiScale(grayscale, 1.3, 5)
+    
+    #4 corrdinates in faces
+    for (x_face, y_face, w_face, h_face) in face:
+        
+        #draw rectangle
+        cv2.rectangle(img, (x_face, y_face), (x_face+w_face, y_face+h_face), (255, 130, 0), 2)
+        
+        #area of intreset in gray image
+        ri_grayscale = grayscale[y_face:y_face+h_face, x_face:x_face+w_face]
+        
+        #area of intrest in cooloured image
+        ri_color = img[y_face:y_face+h_face, x_face:x_face+w_face] 
+        
+        # smile detection , 1.7 is the scale factor , 20 is the min neighbour
+        smile = cascade_smile.detectMultiScale(ri_grayscale, 1.7, 20)
+        
+        #dra rectangle on smile
+        for (x_smile, y_smile, w_smile, h_smile) in smile: 
+            cv2.rectangle(ri_color,(x_smile, y_smile),(x_smile+w_smile, y_smile+h_smile), (255, 0, 130), 2)
+    
+    #return image        
+    return img 
+
+vc = cv2.VideoCapture(0) 
+
+while True:
+    _, img = vc.read() 
+    grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+    final = detection(grayscale, img) 
+    cv2.imshow('Video', final) 
+    string = "pic"+str(datetime.datetime.now())+".jpg"
+    cv2.imwrite(string,final)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break 
+
+vc.release() 
+cv2.destroyAllWindows() 
